41213b6e76a4dc7b32323694cda88c6834c5e2a6
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-12-05 13:33:48 -0700

    getting width and height from file bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
+struct Bitmap {
+    width : u32,
+    height : u32
+}
+
+fn read_4_bytes(bytes : &[u8]) -> u32 {
+    ((bytes[0] as u32) <<  0) +
+    ((bytes[1] as u32) <<  8) +
+    ((bytes[2] as u32) << 16) +
+    ((bytes[3] as u32) << 24)
+}
+
+impl Bitmap {
+    fn from(file_path: &str) -> Result<Bitmap, Box<dyn std::error::Error + 'static>> {
+        let bytes = fs::read(file_path)?;
+        
+        Ok(Bitmap {
+            width : read_4_bytes(&bytes[18..22]),
+            height : read_4_bytes(&bytes[22..26])
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(bmp) = Bitmap::from("image.bmp") {
+        println!("width: {}, height: {}", bmp.width, bmp.height);
+    } else {
+        panic!("what")
+    }
 }
